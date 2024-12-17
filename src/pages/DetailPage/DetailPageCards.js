66a6c54371e43674c5ffed5f458fdcc6dd776185
Basic-정재형 <titochung625l@naver.com>
2024-12-17 22:59:33 +0900

    feat: 관계 UI 컴포넌트화

--- src/pages/DetailPage/DetailPageCards.js
+++ src/pages/DetailPage/DetailPageCards.js
@@ -1,15 +1,51 @@
 import React from 'react';
-import { DetailPageCard, DetailPageCardCreateBtn } from './DetailPage.style';
+import {
+  DetailPageCard,
+  DetailPageCardCreateBtn,
+  DetailPageCardProfile,
+  DetailPageCardProfileImgImage,
+  DetailPageCardProfileImg,
+  DetailPageCardProfileWho,
+  DetailPageCardTextContainer,
+  DetailPageCardDate,
+  DetailPageCardText,
+  DetailPageCardProfileWhoFrom,
+  DetailPageCardProfileWhoRelation,
+} from './DetailPage.style';
 import cardBtn from '../../assets/icons/cardBtn.png';
+import Realtion from './CardRealtion';
 
-function DetailPageCards({ id, cardData }) {
+function DetailPageCards({ id, cardData, onCardClick }) {
   return (
     <>
       <DetailPageCardCreateBtn to={`/post/${id}/message`}>
         <img src={cardBtn} alt="" />
       </DetailPageCardCreateBtn>
       {cardData.map((card) => (
-        <DetailPageCard key={card.id}></DetailPageCard>
+        <DetailPageCard key={card.id} onClick={() => onCardClick(card)}>
+          <DetailPageCardProfile>
+            <DetailPageCardProfileImg>
+              <DetailPageCardProfileImgImage src={card.imageUrl} />
+            </DetailPageCardProfileImg>
+            <DetailPageCardProfileWho>
+              <DetailPageCardProfileWhoFrom>
+                <p className="text-xl font-regular">From.</p>
+                <p className="text-xl font-bold">{card.name}</p>
+              </DetailPageCardProfileWhoFrom>
+              <DetailPageCardProfileWhoRelation>
+                <Realtion relation={card.relation} />
+              </DetailPageCardProfileWhoRelation>
+            </DetailPageCardProfileWho>
+          </DetailPageCardProfile>
+          <DetailPageCardTextContainer>
+            <DetailPageCardText>
+              <p className="text-lg font-regular">{card.text}</p>
+            </DetailPageCardText>
+          </DetailPageCardTextContainer>
+          <DetailPageCardDate>
+            <p className="text-2xs font-regular">{card.date}</p>
+          </DetailPageCardDate>
+        </DetailPageCard>
       ))}
     </>
   );

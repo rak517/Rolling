fceea429f2e7c42e205f4724a6f8a9a7dc913b40
jaehyeongjung <titochung625@naver.com>
2024-12-18 17:40:21 +0900

    feat: detailPage(/post)의 MessageStatusBar UI 구현

---
+++ src/components/layout/MessageStatusBar/MessageStatusBarPresenter.js
@@ -0,0 +1,58 @@
+import {
+  StatusBarContainer,
+  StatusBarWrapper,
+  Title,
+  TitleWrapper,
+  StatusBarSection,
+  StatusBarSectionWrapperLeft,
+  StatusBarSectionWrapperRight,
+  ReactionsWrapper,
+  MessageCount,
+  EmojiDropDown,
+  ButtonWrapper,
+} from './MessageStatusBar.style';
+import ProfileImages from '../../common/ProfileImages';
+import Reactions from '../../common/Reactions';
+import errow_down from '../../../assets/icons/arrow_down.svg';
+import Button from '../../ui/Button';
+import buttonIcon from '../../../assets/icons/button-icon.svg';
+import shareButton from '../../../assets/icons/share-button.svg';
+
+function MessageStatusBarPresenter({ data }) {
+  return (
+    <StatusBarContainer>
+      <StatusBarWrapper>
+        <TitleWrapper>
+          <Title className="text-3xl font-bold">To. {data.name}</Title>
+        </TitleWrapper>
+
+        <StatusBarSection>
+          <StatusBarSectionWrapperLeft>
+            <ProfileImages recentMessages={data.recentMessages}></ProfileImages>
+            <MessageCount className="text-lg">
+              <strong>{data.messageCount}</strong>명이 작성했어요!
+            </MessageCount>
+          </StatusBarSectionWrapperLeft>
+
+          <StatusBarSectionWrapperRight>
+            <ReactionsWrapper>
+              <Reactions topReactions={data.topReactions} noMargin></Reactions>
+              <img src={errow_down} alt="이모지 드롭다운" />
+            </ReactionsWrapper>
+            <ButtonWrapper>
+              <Button size="36" variant="outlined" className="text-base">
+                <EmojiDropDown src={buttonIcon} alt="추가 버튼" />
+                추가
+              </Button>
+            </ButtonWrapper>
+            <Button size="36" variant="outlined">
+              <img src={shareButton} alt="공유 버튼" />
+            </Button>
+          </StatusBarSectionWrapperRight>
+        </StatusBarSection>
+      </StatusBarWrapper>
+    </StatusBarContainer>
+  );
+}
+
+export default MessageStatusBarPresenter;

c93c61371ff55dcc5bc344c67c6bb79f0b8e7143
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2024-12-22 20:46:11 +0900

    refactor: message-status-bar 반응형 UI 및 이모지 드롭다운 기능 수정

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+function useOutsideClick(ref, handler) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        handler();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
+    };
+  }, [ref, handler]);
+}
+
+export default useOutsideClick;

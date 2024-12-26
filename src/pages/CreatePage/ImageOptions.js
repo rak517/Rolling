315827469cc8d675be8308712bdaee0c07a0bf9b
정재형 <jeongjaehyeong@jeongjaehyeong-ui-MacBookAir.local>
2024-12-26 18:38:21 +0900

    fix: createRolling 이미지 오류 해결

--- src/pages/CreatePage/ImageOptions.js
+++ src/pages/CreatePage/ImageOptions.js
@@ -6,11 +6,11 @@ import {
 
 const imageOptions = [
   {
-    url: 'https://s3-alpha-sig.figma.com/img/16cd/1b46/e5a1c8c110aa1788a682f388bef979a4?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VU7Bk3pH8nF7P7IOLYarvLqv~V-G94juaYT-~SdLqI1zY5qlxO9ufV8-l4Mxabm65o2qeMwsNHZpj-HdyHhgaueBkwRo8aKw5udGaBgpRC4HpnMj-IPEGczdNuNBm5XmEsjhZ2nNTYrijysahsFDnpJqdOq9FWgqR-0NHftTYjWZPgyiDVkvttMJvFwFzEwT1rBLrp9X8mnc6B5trSfMcRKyr-k-pFN1ICo7B2eg2n707ZkLRSXliGUzLphagvr27ibKDne~Rhd96W0zP3~0hWnGlsGaoBynIBnqsJC4uSuEuo-xXg0kD6pcwZr1TZzIkV~-2ue4O-Mx7NvaGzSZsQ__',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf_mr6dx1OYsdjbGStsFqWdWZlTUWe-jpQcg&s',
     label: '이미지 1',
   },
   {
-    url: 'https://s3-alpha-sig.figma.com/img/20c6/e481/31612bf263d24dd830adf62fd00509ed?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=frJ5~XixhYls1hSAd-iBR1C3xSdEsm99C8MQW1gMlH0T7OrySXoPjMUUom2yoaN1sqFUTvvSaqPmAo6PfnueNJHWxUTX0-5bLumdB69nqNVKfjKMWuKFMP4obXLBCT-GsnPACcVZtRO3tSHBG-hA7op3Ft-Ps6ORvqNwVActxeNCuJBjEDNeZY5TBMXEQm8bZeUvuTji8PX6L4WjF5GO92ftiuvDbcAAzM9hiTkZ~--FFex1IoEB86IJU6oSXEQ5o9xK8sHG5SlnBarO-yge6Z0u-v1dSYMTXKAAYpFPfIUCdKmMpdTNLJrA-e0inMjdvw0oR-29Rq28ZCV6BwiUDw__',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsBlbGhLfS6zHpGdWtXiCh5I0TWpB4YIJbPA&s',
     label: '이미지 2',
   },
   {

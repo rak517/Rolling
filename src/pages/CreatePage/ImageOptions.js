5e998b2a33ce99689c508a60507fbd69d14c924c
jaehyeongjung <titochung625@naver.com>
2024-12-26 19:23:41 +0900

    refactor: img 해상도 1920x1080 변경

--- src/pages/CreatePage/ImageOptions.js
+++ src/pages/CreatePage/ImageOptions.js
@@ -6,19 +6,19 @@ import {
 
 const imageOptions = [
   {
-    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf_mr6dx1OYsdjbGStsFqWdWZlTUWe-jpQcg&s',
+    url: 'https://preview.redd.it/nhk8jg3psng71.jpg?auto=webp&s=0614423637af7440921c3932759d8a90a24981fb',
     label: '이미지 1',
   },
   {
-    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsBlbGhLfS6zHpGdWtXiCh5I0TWpB4YIJbPA&s',
+    url: 'https://preview.redd.it/fvaecdifsng71.jpg?auto=webp&s=5b58fd8b3baf37248db2253b5bb7b3a7f5a1815f',
     label: '이미지 2',
   },
   {
-    url: 'https://cdn.pixabay.com/photo/2017/09/16/15/49/sea-2755858_1280.jpg',
+    url: 'https://preview.redd.it/fire-and-frost-a-winter-tale-in-v%C3%A4rmland-1920x1080-v0-6sq3a0f3q37e1.jpeg?auto=webp&s=ea9dbb178557ff63149c1e86cc92d3886c1533ac',
     label: '이미지 3',
   },
   {
-    url: 'https://cdn.pixabay.com/photo/2023/06/04/23/09/desert-8041046_1280.jpg',
+    url: 'https://external-preview.redd.it/cFsTudt5cj4sW8Yi_IQS7GCxZa0Twt6WUK4av0XvWLs.jpg?auto=webp&s=a4f4dc9d7bb78f72414d81f8843d4c26a5348ecb',
     label: '이미지 4',
   },
 ];
